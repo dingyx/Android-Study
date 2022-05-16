7aa61eb1b91e6d51aa12f9bfb473c131b16ad4ae
dingyx <dyx1222@gmail.com>
2022-05-16 17:11:29 +0800

    图形绘制

--- view-study/src/main/java/com/sycamore/viewstudy/transformview/MatrixTranslateView.java
+++ view-study/src/main/java/com/sycamore/viewstudy/transformview/MatrixTranslateView.java
@@ -1,11 +1,16 @@
 package com.sycamore.viewstudy.transformview;
 
 import android.content.Context;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
 import androidx.annotation.Nullable;
+
+import com.sycamore.viewstudy.utils.ViewUtils;
 
 /**
  * @author dingyx
@@ -19,15 +24,30 @@ import androidx.annotation.Nullable;
  */
 public class MatrixTranslateView extends View {
 
+    Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
+    Matrix matrix = new Matrix();
 
 
     public MatrixTranslateView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
     }
 
+    {
+        matrix.reset();
+        matrix.postTranslate(100,200);
+        matrix.postRotate(90,100,200);
+    }
+
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
+        canvas.save();
+        canvas.concat(matrix);
+        canvas.drawBitmap(ViewUtils.getAvatar(getResources(), 120), 0, 0, paint);
+        canvas.restore();
+
     }
 
 
